84f1cd9ee49e9cf67e9c3874603da9a2dcad5dc6
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2023-10-17 13:48:32 +0100

    Project - Contract composition made.

--- src/org/ulpgc/is1/model/Contract.java
+++ src/org/ulpgc/is1/model/Contract.java
@@ -4,6 +4,36 @@ import java.util.Date;
 
 public class Contract {
     private Date start;
-    private int budget;
     private Date end;
+    private int budget;
+
+    public Contract(Date start, Date end, int budget) {
+        this.start = start;
+        this.end = end;
+        this.budget = budget;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
 }
