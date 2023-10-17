af800d13260ec2c2c29c6873f52267a8205e4e6b
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2023-10-17 10:21:24 +0100

    Project aggregations made. EMPLOYEE CONNECTIONS PROTOTYPE.

---
+++ src/org/ulpgc/is1/model/Effort.java
@@ -0,0 +1,37 @@
+package org.ulpgc.is1.model;
+
+public class Effort {
+    private Employee employee;
+    private Task task;
+    private int amount;
+
+    public Effort(Employee employee, Task task, int amount) {
+        this.employee = employee;
+        this.task = task;
+        this.amount = amount;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
--- src/org/ulpgc/is1/model/Employee.java
+++ src/org/ulpgc/is1/model/Employee.java
@@ -1,13 +1,63 @@
 package org.ulpgc.is1.model;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Employee {
     private String name;
     private String email;
-    private ArrayList<Project> developer;
+    private ArrayList<Project> developers;
     private ArrayList<Task> tasks;
-    private Project manager;
+    private ArrayList<Project> projects;
 
+    public Employee(String name, String email, ArrayList<Project> developer, ArrayList<Task> tasks, ArrayList<Project> projects) {
+        this.name = name;
+        this.email = email;
+        this.developers = new ArrayList<Project>();
+        this.tasks = tasks;
+        this.projects = projects;
+    }
+
+    void addTask(Task task){
+        tasks.add(task);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Project> getDevelopers() {
+        return developers;
+    }
+
+    public void setDevelopers(ArrayList<Project> developers) {
+        this.developers = developers;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(ArrayList<Project> projects) {
+        this.projects = projects;
+    }
 }
