0e3830665aab4e190bf9818b07b133fc97eeac27
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2023-10-17 14:13:26 +0100

    Classes Customer & Phone (isValid() method) made.

--- src/org/ulpgc/is1/model/Customer.java
+++ src/org/ulpgc/is1/model/Customer.java
@@ -1,4 +1,38 @@
 package org.ulpgc.is1.model;
 
 public class Customer {
+    private String name;
+    private String surname;
+    private Phone phone;
+
+    public Customer(String name, String surname, Phone phone) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+    }
+
+    public void setPhone(Phone number){
+        this.phone = number;
+        this.phone.isValid();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
 }
--- src/org/ulpgc/is1/model/Phone.java
+++ src/org/ulpgc/is1/model/Phone.java
@@ -2,4 +2,22 @@ package org.ulpgc.is1.model;
 
 public class Phone {
     private String number;
+
+    public Phone(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void isValid() {
+        if (!number.matches("\\d{9}")) {
+            number = "XXXX";
+        }
+    }
 }
